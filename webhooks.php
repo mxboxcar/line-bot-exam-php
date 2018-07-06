bbdb753c4aff602829c4c626e268be73b728fd9e
mxboxcar <mx.boxcar@gmail.com>
2018-07-06 15:26:04 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,10 +23,10 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $event
 			];
 			
-			print_r($event);
+			//print_r($event);
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
