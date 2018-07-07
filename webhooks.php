c54d267e48024810901a292935e09fe3be575b12
mx.boxcar@gmail.com <mx.boxcar@gmail.com>
2018-07-07 12:10:32 +0700

    x

--- webhooks.php
+++ webhooks.php
@@ -63,9 +63,6 @@ function register($code, $uid) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL,$strUrl);
     curl_setopt($ch, CURLOPT_HEADER, false);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
-    curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     $result = curl_exec($ch);
