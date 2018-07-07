ea011696032a92f143e149937d718800bfe23811
mxboxcar <mx.boxcar@gmail.com>
2018-07-07 12:14:11 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -26,14 +26,14 @@ if (strpos($message, 'code=[') !== false) {
 	$exp = explode('code=[' , $message);
 	$exp2 = explode(']', $exp[1]);
 
-    register($exp2[0], $arrayJson['source']['userId'])
+//register($exp2[0], $arrayJson['source']['userId'])
 
 	$arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
         $arrayPostData['messages'][0]['text'] = "
 ได้ทำการลงทะเบียนสำหรับโทรศัพท์เครื่องนี้แล้ว
 	
-รหัสลูกค้าของคุณคือ ".$exp2[0].$data['id']."
+รหัสลูกค้าของคุณคือ ".$exp2[0].$arrayJson['source']['userId']."
 คุณจะได้รับการแจ้งเตือนจากระบบโดยอัตโนมัติผ่านช่องทาง Line นี้
 การตอบกลับ จะไม่สามารถทำได้ กรุณาตอบกลับที่ Line : @ydcargo
 คุณสามารถลงทะเบียนใหม่อีกครั้ง โดยพิมพ์ code=[รหัสลูกค้าของคุณ]
